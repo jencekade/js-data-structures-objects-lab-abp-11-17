083d96a7baaee8bfb0cfd0fab8f1623dbc46d0d5
jencekade <gaephuoc@gmail.com>
2017-11-08 08:52:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,15 +11,14 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return driver;
 }
 
-/*function deleteFromDriverByKey(driver, key) {
-
-  let newDriver1 = {...driver};
-  delete driver.key;
+function deleteFromDriverByKey(driver, key) {
+  let newDriver1 = Object.assign({}, driver, { [key]: ""});
+  delete newDriver1.key;
   return newDriver1;
   //const newObj = Object.assign({}, driver);
   //return newObj;
 }
-*/
+
 function destructivelyDeleteFromdriverByKey(driver, key) {
   delete driver.key;
   return driver;
